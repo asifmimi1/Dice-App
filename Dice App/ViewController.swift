f7ea9a954b35ef7293347ce01ac5fe46f457fcb6
ASIF MIMI <asif.mimi1@gmail.com>
2019-10-28 21:24:04 +0600

    Dice App: 1. Complete AutoLayout 2. Scroll View

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var imageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    var randomImage1: Int = 0
+    var randomImage2: Int = 0
+    
+    @IBOutlet weak var diceLogo_1: UIImageView!
+    @IBOutlet weak var diceLogo_2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //diceLogo()
     }
-
+    @IBAction func rollButton(_ sender: UIButton) {
+       diceLogo()
+    }
+    
+    func diceLogo() {
+        randomImage1 = Int.random(in: 0 ... 5)
+        randomImage2 = Int.random(in: 0 ... 5)
+        diceLogo_1.image = UIImage.init(named: imageArray[randomImage1])
+        diceLogo_2.image = UIImage.init(named: imageArray[randomImage2])
+    }
 
 }
 
