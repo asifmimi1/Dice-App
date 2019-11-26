5a9d0d58b15cbab20203be2954e90aec99623621
ASIF MIMI <asif.mimi1@gmail.com>
2019-11-26 14:29:07 +0600

    1. Text label added with "lucky charm"

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -14,10 +14,12 @@ class ViewController: UIViewController {
     var imageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     var randomImage1: Int = 0
     var randomImage2: Int = 0
+    var textLabel : String = ("You got a lucky charm")
     
     @IBOutlet weak var diceLogo_1: UIImageView!
     @IBOutlet weak var diceLogo_2: UIImageView!
     
+    @IBOutlet weak var textView: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +38,12 @@ class ViewController: UIViewController {
         randomImage2 = Int.random(in: 0 ... 5)
         diceLogo_1.image = UIImage.init(named: imageArray[randomImage1])
         diceLogo_2.image = UIImage.init(named: imageArray[randomImage2])
+        
+        if diceLogo_1.image == diceLogo_2.image{
+            textView.text = textLabel
+        }else{
+            textView.text = ""
+        }
     }
 
 }
